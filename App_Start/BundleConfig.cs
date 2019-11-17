121753e75b5c678ba126fed3c94a77d3bb7e451a
Stephane Pouwels <s.pouwels@laposte.net>
2019-11-17 17:25:27 +0100

    4th commit

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,11 +8,15 @@ namespace Modells
         // Pour plus d'informations sur le regroupement, visitez https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // Add jquery script in the bundle :
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
+            // Add jqueryval script in the bundle :
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+            // Add popper script in the bundle :
+            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
+                   "~/Scripts/umd/popper.js"));
 
             // Utilisez la version de développement de Modernizr pour le développement et l'apprentissage. Puis, une fois
             // prêt pour la production, utilisez l'outil de génération à l'adresse https://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
