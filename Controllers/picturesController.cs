f19c373a021fad585a39b7d0c4b8fd8bd7d65ae4
Stephane Pouwels <s.pouwels@laposte.net>
2020-01-24 16:39:41 +0100

    commit n°11

--- Controllers/picturesController.cs
+++ Controllers/picturesController.cs
@@ -15,7 +15,10 @@ namespace Modells.Controllers
     {
         private picturesModells db = new picturesModells();
 
-
+       /// <summary>
+       /// List of pictures.
+       /// </summary>
+       /// <returns></returns>
         public ActionResult Index()
         { 
             // Picture list :
@@ -24,12 +27,19 @@ namespace Modells.Controllers
             return View(picture.ToList());
         }
 
-        // GET: pictures
+        /// <summary>
+        /// Pictures list with pagination.
+        /// </summary>
+        /// <param name="pageToDisplay"></param>
+        /// <returns></returns>
         public ActionResult Index2(int? pageToDisplay)
         {
+            #region Get Pictures List
             // Picture list :
             var picture = db.picture.Include(p => p.category);
+            #endregion
 
+            #region Generate pagination
             // Total number of pictures :
             var numberOfTotalPics = picture.Count();
 
@@ -54,6 +64,7 @@ namespace Modells.Controllers
             // Viewbags to pass data in the view :
             ViewBag.totalPictures = numberOfPicturesCurrentPage;
             ViewBag.totalPages = numberOfPages;
+            #endregion
 
             return View(pictures);
         }
@@ -87,37 +98,47 @@ namespace Modells.Controllers
             return View();
         }
 
-
-        // POST: pictures/Create
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Create a new picture - Save the source in the directory & properties in the database.
+        /// </summary>
+        /// <param name="newPicture"></param>
+        /// <param name="newPictureToUpload"></param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult pictureCreate([Bind(Include = "pictureId,pictureTitle,pictureAlternateTitle,pictureDescription,pictureStandardUrl,categoryId")] picture picture, HttpPostedFileBase pictureToUpload)
+        public ActionResult pictureCreate([Bind(Include = "pictureId,pictureTitle,pictureAlternateTitle,pictureDescription,pictureStandardUrl,categoryId")] picture newPicture, HttpPostedFileBase newPictureToUpload)
         {
+            #region Dropdownlist
+            // Display a dropdownlist for the picture categories in the view :
+            ViewBag.categoryId = new SelectList(db.category, "categoryId", "categoryName", newPicture.categoryId);
+            #endregion
 
-            if (pictureToUpload != null)
+            #region Directory Save
+            // Add & save picture file to directory :
+            if (newPictureToUpload != null)
             {
                 // Picture name :
-                string pictureSourceName = Path.GetFileName(pictureToUpload.FileName);
+                string newPictureSourceName = Path.GetFileName(newPictureToUpload.FileName);
 
                 // Combine directory path & picture name to make the source picture :
-                string pictureSourcePath = Path.Combine(Server.MapPath("~/Content/Images/Pictures/"), pictureSourceName);
+                string newPictureSourcePath = Path.Combine(Server.MapPath("~/Content/Images/Pictures/"), newPictureSourceName);
 
                 // Picture Source is uploaded and saved in the directory :
-                pictureToUpload.SaveAs(pictureSourcePath);
-
+                newPictureToUpload.SaveAs(newPictureSourcePath);
             }
+            #endregion
 
-                if (ModelState.IsValid)
+            #region Database Save
+            // Add & Save picture entity to database :
+            if (ModelState.IsValid)
             {
-                db.picture.Add(picture);
+                db.picture.Add(newPicture);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+            #endregion
 
-            ViewBag.categoryId = new SelectList(db.category, "categoryId", "categoryName", picture.categoryId);
-            return View(picture);
+            return View(newPicture);
         }
 
         // POST: pictures/Create
@@ -136,25 +157,6 @@ namespace Modells.Controllers
 
             ViewBag.categoryId = new SelectList(db.category, "categoryId", "categoryName", picture.categoryId);
             return View(picture);
-        }
-
-        // Enable to upload the picture source to the appropriate directory :
-        public ActionResult PictureSourceUpload(HttpPostedFileBase pictureToUpload)
-        {
-            if (pictureToUpload != null)
-            {
-                // Picture name :
-                string pictureSourceName = Path.GetFileName(pictureToUpload.FileName);
-
-                // Combine directory path & picture name to make the source picture :
-                string pictureSourcePath = Path.Combine(Server.MapPath("~/Content/Images/Pictures/"), pictureSourceName);
-                
-                // Picture Source is uploaded :
-                pictureToUpload.SaveAs(pictureSourcePath);
-            }
-            
-            // After a successful upload - redirect the u
-            return RedirectToAction("pictureCreate", "Pictures");
         }
 
         // GET: pictures/Edit/5
